f7ac558494fa286249ffa95edbfcded5c5c62aca
cda75 <dchestnov@mail.ru>
2016-06-17 09:23:56 +0000

    add shade install

--- mesos_install.sh
+++ mesos_install.sh
@@ -10,7 +10,7 @@ echo "Waiting while all node services started"
 sleep 40
 
 #Updating Ansible hosts file
-source /home/ansible/keystonerc_admin
+source keystonerc_
 echo "[mesos_master]" >> /etc/ansible/hosts
 nova --os-project-name=Mesos list | grep Master | awk '{print $13}' >> /etc/ansible/hosts
 echo "" >> /etc/ansible/hosts
@@ -18,14 +18,14 @@ echo "[mesos_slave]" >> /etc/ansible/hosts
 nova --os-project-name=Mesos list | grep Slave | awk '{print $13}' >> /etc/ansible/hosts
 
 #Get all ip-s in one file for future processing
-nova --os-project-name=Mesos list | grep -P '\d' | awk '{print $13}' > /etc/ansible/mesos/all.ip
+nova --os-project-name=Mesos list |  awk '{print $4"=",$13}' > /etc/ansible/mesos/floating.ip
 
 #Clear known_hots file from existing host keys
-while read line; do
-    ssh-keygen -f "/root/.ssh/known_hosts" -R $line
-    ssh-keyscan -H $line >> ~/.ssh/known_hosts
-done < all.ip
-rm -f all.ip
+#while read line; do
+#    ssh-keygen -f "/root/.ssh/known_hosts" -R $line
+#    ssh-keyscan -H $line >> ~/.ssh/known_hosts
+#done < all.ip
+#rm -f all.ip
 
 #Update host files on node VMs
 ansible-playbook update_hosts.yaml
@@ -38,5 +38,6 @@ ansible-playbook master_install.yaml
 
 #Deploy Slave node software
 ansible-playbook slave_install.yaml
+
 
 
