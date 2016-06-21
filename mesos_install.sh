1ca14fd0c8ebf2e7f2581de71f50bee75f819506
cda75 <dchestnov@mail.ru>
2016-06-21 11:40:46 +0000

    almost finished 2

--- mesos_install.sh
+++ mesos_install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]]; then
+  echo 
+  echo "================================================================"
+  echo "                  NO ARGUMENTS PROVIDED"
+  echo 
+  echo "Usage:"
+  echo " ./mesos_install.sh single  -  installs single Mesos Master node"
+  echo " ./mesos_install.sh cluster -  installs 3x Mesos Master cluster "
+  echo "================================================================"
+  echo 
+  exit 1
+fi
+
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 # Creating VM instances
-ansible-playbook create_vm.yaml
+ansible-playbook create_vm.yaml --extra-vars "ha=$1"
 
 #Wait for VM running
-echo "Waiting while all node services started"
+echo " Waiting while nodes started"
 sleep 40
-
-#Updating Ansible hosts file
-source keystonerc_
-echo "[mesos_master]" >> /etc/ansible/hosts
-nova --os-project-name=Mesos list | grep Master | awk '{print $13}' >> /etc/ansible/hosts
-echo "" >> /etc/ansible/hosts
-echo "[mesos_slave]" >> /etc/ansible/hosts
-nova --os-project-name=Mesos list | grep Slave | awk '{print $13}' >> /etc/ansible/hosts
 
 #Clear known_hots file from existing host keys
 nova --os-project-name=Mesos list | grep -P '\d' | awk '{print $13}' > all.ip
@@ -26,7 +31,7 @@ done < all.ip
 rm -f all.ip
 
 #Add hosts ip to /etc/hosts file
-ansible-playbook update_hosts_file.yaml
+ansible-playbook update_ansible_hosts.yaml
 
 #Deploy common configuration
 ansible-playbook common_install.yaml
@@ -37,10 +42,13 @@ ansible-playbook master_install.yaml
 #Deploy Slave node software
 ansible-playbook slave_install.yaml
 
+#Install dockers on Slave nodes
+ansible-playbook docker_install.yaml
+
 #Reboot all mesos hosts
 ansible-playbook reboot_all.yaml
 
-mesos_ip=`nova --os-project-name=Mesos list |  grep 'Master-1' | awk '{print $13}'` 
+mesos_ip=`nova --os-project-name=Mesos list |  grep 'Master' | awk '{print $13}'` 
 echo "===================================================="
 echo " Mesos cluster running on:     http://$mesos_ip:5050"
 echo "===================================================="
